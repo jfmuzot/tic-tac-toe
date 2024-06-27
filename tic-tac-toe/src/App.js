cfc7cfc968f00a260a5b1d10030dc3c0d6ef51dd
jfmuzot <jfmuzot@puce.edu.ec>
2024-06-26 23:45:41 -0500

    segundo react

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Square from './componentes/square';
 function App() {
   return (
     <div className="App">
-      
+      <Square />
     </div>
   );
 }
